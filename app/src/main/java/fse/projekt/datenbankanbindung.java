f8492f1d53251eec98a06101101c1a67c6558e29
PJL <pauljohannlorenz@proton.me>
2024-12-01 10:34:44 +0100

    Initial commit

---
+++ app/src/main/java/fse/projekt/datenbankanbindung.java
@@ -0,0 +1,38 @@
+package fse.projekt;
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class datenbankanbindung {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://gruppeFSE:NRafLaCNWFo2C4rN@clusterfse.m0lk3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFSE";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
